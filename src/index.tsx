b40e7a533f481cd367bb578e9ea85a71d0c916e6
Ryan Pedersen <ryan.pedersen@microsoft.com>
2019-07-23 16:14:43 -0700

    index changed too

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Hello from './components/Hello';
+import Hello from './components/StatefulHello';
 
 ReactDOM.render(<Hello name="TypeScript" enthusiasmLevel={10} />, document.getElementById('root'));
 
