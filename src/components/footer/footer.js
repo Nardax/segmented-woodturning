614a59f5b3462ddc3d38916534a00823d4e8c4d0
Ryan Pedersen <34652922+Nardax@users.noreply.github.com>
2020-05-27 07:24:46 -0700

    first go of it - not good (#6)
    basic layout
    
    Co-authored-by: Ryan Pedersen <ryan.pedersen@microsoft.com>

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>&copy; 2020 Ryan Pedersen</div>
+        <footer>&copy; 2020 Ryan Pedersen</footer>
     );
 }
 
--- src/components/ring/ring.js
+++ src/components/ring/ring.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Ring = props => {
+const Ring = (props) => {
+    const [segments, setSegments] = useState(10)
+
+    const addSegment = () => {
+        setSegments(segments + 1);
+    }
+
+    const deleteSegment = () => {
+        setSegments(segments - 1);
+    }
+
     return (
-        <div key={props.key}>{props.text}</div>
+        <div className="ring">
+            <div className="ring-title">Ring {props.id}</div>
+            <div>
+                Segments:
+                <button onClick={addSegment}>+</button>
+                <input type="number" id="segments" value={segments} />
+                <button onClick={deleteSegment}>-</button>
+            </div>
+        </div>
     );
 }
 
