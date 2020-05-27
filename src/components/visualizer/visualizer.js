8195148d5d11945180ce5c5289dce736c6d87146
Ryan Pedersen <ryan.pedersen@microsoft.com>
2020-05-27 07:28:34 -0700

    add packages
    adding scene component package
    
    adding box to scene

--- src/components/visualizer/visualizer.js
+++ src/components/visualizer/visualizer.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import { FreeCamera, Vector3, HemisphericLight, MeshBuilder } from '@babylonjs/core'
+import SceneComponent from 'babylonjs-hook';
+
+let box;
+
+const onSceneReady = scene => {
+  var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+  camera.setTarget(Vector3.Zero());
+
+  const canvas = scene.getEngine().getRenderingCanvas();
+
+  camera.attachControl(canvas, true);
+
+  var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+  light.intensity = 0.7;
+
+  box = MeshBuilder.CreateBox("box", {size: 2}, scene);
+  box.position.y = 1;
+
+  MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+}
+
+const onRender = scene => {
+  if (box !== undefined) {
+    var deltaTimeInMillis = scene.getEngine().getDeltaTime();
+
+    const rpm = 10;
+    box.rotation.y += ((rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000));
+  }
+}
 
 const Visualizer = () => {
     return (
-        <div className="visualizer">3D model goes here</div>
+        <SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} className="visualizer" />
     );
 }
 
