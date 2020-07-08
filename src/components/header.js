2197999e516edeea389d4ea0114721582b2026d4
alexrpedersen <alex.r.pedersen@outlook.com>
2020-07-07 17:35:24 -0700

    adds button to header but blocked by the segment display

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Facebook from '../backend/auth/facebook-auth/facebook-auth';
 
 const Header = () => {
     return (
         <header>
             <h1>Segmented Woodturning</h1>
+            {/* <Facebook />  */}
         </header>
+
     );
 }
 
