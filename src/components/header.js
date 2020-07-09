fa129d4ef8be6cd1570534b48152a42cc91561ad
Alex Pedersen <59542396+alexrpedersen@users.noreply.github.com>
2020-07-09 16:27:08 -0700

    completely finished with facebook auth and with working button (#26)
    adds button to header but blocked by the segment display
    
    adds react-facebook to dependencies
    
    updated auth to hooks

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Facebook from './auth';
 
 const Header = () => {
     return (
         <header>
             <h1>Segmented Woodturning</h1>
+            {/* <Facebook />  */}
         </header>
+
     );
 }
 
