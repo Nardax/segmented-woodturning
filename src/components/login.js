720b679c0bac2ce62e525f877ca2af7b6fe884a2
alexrpedersen <alex.r.pedersen@outlook.com>
2020-08-27 22:35:32 -0700

    rework

--- src/components/login.js
+++ src/components/login.js
@@ -1,34 +1,108 @@
 import React from 'react';
-import Dialog from 'react-dialog';
+import ReactModalLogin from 'react-modal-login';
+import {facebookConfig, googleConfig} from "social-config";
+
+class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false,
+      loading: false,
+      error: null
+    };
+
+  }
+
+  openModal() {
+    this.setState({
+      showModal: true,
+    });
+  }
+
+  closeModal() {
+    this.setState({
+      showModal: false,
+      error: null
+    });
+  }
+  
+  onLoginSuccess(method, response) {
+    console.log('logged successfully with ' + method);
+  }
+
+  onLoginFail(method, response) {
+    console.log('logging failed with ' + method);
+    this.setState({
+      error: response
+    })
+  }
+
+  startLoading() {
+    this.setState({
+      loading: true
+    })
+  }
+
+  finishLoading() {
+    this.setState({
+      loading: false
+    })
+  }
+
+  onTabsChange() {
+    this.setState({
+      error: null
+    });
+  }
 
 
- 
-    const openDialog = () => this.setState({ isDialogOpen: true })
- 
-    const handleClose = () => this.setState({ isDialogOpen: false })
- 
-    const Login = () => { 
-        return (
-            <div className="container">
-                <button type="button" onClick={this.openDialog}>Login</button>
-                {
-                    this.state.isDialogOpen &&
-                    <Dialog
-                        title="Login"
-                        modal={true}
-                        onClose={this.handleClose}
-                        buttons={
-                            [{
-                                text: "Close",
-                                onClick: () => this.handleClose()
-                            }]
-                        }>
-                        <h1>Dialog Content</h1>
-                        <p>More Content. Anything goes here</p>
-                    </Dialog>
-                }
-            </div>
-        );
-    }
+  render() {
+
+    return (
+      <div>
+
+        <button
+          onClick={() => this.openModal()}
+        >
+          Open Modal
+        </button>
+
+        <ReactModalLogin
+          visible={this.state.showModal}
+          onCloseModal={this.closeModal.bind(this)}
+          loading={this.state.loading}
+          error={this.state.error}
+          tabs={{
+            onChange: this.onTabsChange.bind(this)
+          }}
+          loginError={{
+            label: "Couldn't sign in, please try again."
+          }}
+          registerError={{
+            label: "Couldn't sign up, please try again."
+          }}
+          startLoading={this.startLoading.bind(this)}
+          finishLoading={this.finishLoading.bind(this)}
+          providers={{
+            facebook: {
+              config: facebookConfig,
+              onLoginSuccess: this.onLoginSuccess.bind(this),
+              onLoginFail: this.onLoginFail.bind(this),
+              label: "Continue with Facebook"
+            },
+            google: {
+              config: googleConfig,
+              onLoginSuccess: this.onLoginSuccess.bind(this),
+              onLoginFail: this.onLoginFail.bind(this),
+              label: "Continue with Google"
+            }
+          }}
+        />
+      </div>
+    )
+  }
+}
 
 export default Login;
