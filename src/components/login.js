21fd0bd7e2f9a290a3e8bea9e5835159201c7bfe
alexrpedersen <alex.r.pedersen@outlook.com>
2020-08-28 18:29:32 -0700

    added hooks

--- src/components/login.js
+++ src/components/login.js
@@ -2,107 +2,106 @@ import React from 'react';
 import ReactModalLogin from 'react-modal-login';
 import {facebookConfig, googleConfig} from "social-config";
 
-class Login extends React.Component {
+function Sample() {
+  // replace constructor w/ a `useState`-ish
+  const [
+    shouldShowModal,
+    setShouldShowModal,
+    isLoading,
+    setIsLoading,
+    error,
+    setError,
+  ] = useLogin(null);
 
-  constructor(props) {
-    super(props);
+  // so each individual useEffect can list the props is subscribes to as its second arg (after the closure)
+  // alternatively, you could probably combine these into one effect
+  useEffect(() => {
+    function openModal() {
+      setShouldShowModal(true);
+    }
+  });
 
-    this.state = {
-      showModal: false,
-      loading: false,
-      error: null
-    };
+  useEffect(() => {
+    function closeModal() {
+      setShouldShowModal(false);
+      setError(null);
+    }
+  });
+    
+  useEffect(() => {
+    function onLoginSuccess(method) {
+      console.log('logged successfully with ' + method);
+    }
+  });
 
-  }
+  useEffect(() => {
+    function onLoginFail(method, response) {
+      console.log('logging failed with ' + method);
+      setError(response);
+    }
+  });
 
-  openModal() {
-    this.setState({
-      showModal: true,
-    });
-  }
+  useEffect(() => {
+    function startLoading() {
+      setIsLoading(true);
+    }
+  });
 
-  closeModal() {
-    this.setState({
-      showModal: false,
-      error: null
-    });
-  }
-  
-  onLoginSuccess(method, response) {
-    console.log('logged successfully with ' + method);
-  }
+  useEffect(() => {
+    function finishLoading() {
+      setIsLoading(false);
+    }
+  });
 
-  onLoginFail(method, response) {
-    console.log('logging failed with ' + method);
-    this.setState({
-      error: response
-    })
-  }
+  useEffect(() => {
+    function onTabsChange() {
+      setError(null);
+    }
+  });
 
-  startLoading() {
-    this.setState({
-      loading: true
-    })
-  }
+  // I think something is wrong w/ this return syntax
+  return (
+    <div>
 
-  finishLoading() {
-    this.setState({
-      loading: false
-    })
-  }
+      <button
+        onClick={() => this.openModal()}
+      >
+        Open Modal
+      </button>
 
-  onTabsChange() {
-    this.setState({
-      error: null
-    });
-  }
-
-
-  render() {
-
-    return (
-      <div>
-
-        <button
-          onClick={() => this.openModal()}
-        >
-          Open Modal
-        </button>
-
-        <ReactModalLogin
-          visible={this.state.showModal}
-          onCloseModal={this.closeModal.bind(this)}
-          loading={this.state.loading}
-          error={this.state.error}
-          tabs={{
-            onChange: this.onTabsChange.bind(this)
-          }}
-          loginError={{
-            label: "Couldn't sign in, please try again."
-          }}
-          registerError={{
-            label: "Couldn't sign up, please try again."
-          }}
-          startLoading={this.startLoading.bind(this)}
-          finishLoading={this.finishLoading.bind(this)}
-          providers={{
-            facebook: {
-              config: facebookConfig,
-              onLoginSuccess: this.onLoginSuccess.bind(this),
-              onLoginFail: this.onLoginFail.bind(this),
-              label: "Continue with Facebook"
-            },
-            google: {
-              config: googleConfig,
-              onLoginSuccess: this.onLoginSuccess.bind(this),
-              onLoginFail: this.onLoginFail.bind(this),
-              label: "Continue with Google"
-            }
-          }}
-        />
-      </div>
-    )
-  }
+      <ReactModalLogin
+        visible={this.state.showModal}
+        onCloseModal={this.closeModal.bind(this)}
+        loading={this.state.loading}
+        error={this.state.error}
+        tabs={{
+          onChange: this.onTabsChange.bind(this)
+        }}
+        loginError={{
+          label: "Couldn't sign in, please try again."
+        }}
+        registerError={{
+          label: "Couldn't sign up, please try again."
+        }}
+        startLoading={this.startLoading.bind(this)}
+        finishLoading={this.finishLoading.bind(this)}
+        providers={{
+          facebook: {
+            config: facebookConfig,
+            onLoginSuccess: this.onLoginSuccess.bind(this),
+            onLoginFail: this.onLoginFail.bind(this),
+            label: "Continue with Facebook"
+          },
+          google: {
+            config: googleConfig,
+            onLoginSuccess: this.onLoginSuccess.bind(this),
+            onLoginFail: this.onLoginFail.bind(this),
+            label: "Continue with Google"
+          }
+        }}
+      />
+    </div>
+  )
 }
 
 export default Login;
