27f06ffa31d13f8a24d7459070878d048d6e53d2
alexrpedersen <alex.r.pedersen@outlook.com>
2020-09-10 12:40:06 -0700

    added login functionality

--- src/components/login.js
+++ src/components/login.js
@@ -2,6 +2,17 @@ import React, { useState } from 'react';
 import Auth from '../components/auth';
 import Modal from 'react-modal';
 
+const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)'
+    }
+  };
+
 function Login () {
 
     const [modalIsOpen, setModalIsOpen] = useState(false)
@@ -9,9 +20,10 @@ function Login () {
     return (
         <div className='Login'>
             <button onClick={() => setModalIsOpen(true)}>Login</button>
-            <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+            <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} style={customStyles}>
                 <h2>Login</h2>
-                <p>Sign in buttons</p>
+                <Auth >
+                </Auth>
                 <div>
                     <button onClick={() => setModalIsOpen(false)}>Close</button>
                 </div>
