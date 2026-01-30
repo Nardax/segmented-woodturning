dea33ec0daced4fae941d6a3fed7872efcef53d4
Ryan Pedersen <ryan.pedersen@outlook.com>
2026-01-30 14:26:17 -0500

    removing old version

--- README.md
+++
@@ -1,5 +0,0 @@
-# Segmented Woodturning
-
-An application used to plan segmented woodturning projects.
-
-The existing version can be used at https://segmented-woodturning.azurewebsites.net/
--- src/redux/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const ADD_RING = 'ADD_RING';
-export const DELETE_RING = 'DELETE_RING';
-export const UPDATE_RING = 'UPDATE_RING';
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
